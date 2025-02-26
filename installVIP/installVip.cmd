8ee3c50d4aa359201a677e76de6a37af470ca07d
Erik Partridge <erik.partridge@pdc.com>
2025-02-26 12:54:39 +0100

    cleanup

--- installVIP/installVip.cmd
+++
@@ -1,6 +0,0 @@
-cd %0/../
-echo Latest version is available on the pdc_vip repository, under 'Artifacts'
-set installPath="C:\Program Files (x86)\Visual Prolog Build1100 Step5"
-mkdir %installPath%
-pause
-npm install --prefix %installPath% "visual-prolog-11.0.5-3.tgz"
